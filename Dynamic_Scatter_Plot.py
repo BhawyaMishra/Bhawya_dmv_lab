d6efde4bf90d416af86269d31d5a46a6f5f01658
BhawyaMishra <151458045+BhawyaMishra@users.noreply.github.com>
2026-02-24 12:01:38 +0530

    Update Dynamic_Scatter_Plot.py

--- Dynamic_Scatter_Plot.py
+++ Dynamic_Scatter_Plot.py
@@ -2,18 +2,16 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import random
 
-# Create figure and axis
 fig, ax = plt.subplots()
 
 x_data = []
 y_data = []
 
 def update(frame):
-    # Add new random data points
     x_data.append(random.randint(0, 50))
     y_data.append(random.randint(0, 100))
     
-    ax.clear()  # Clear previous frame
+    ax.clear()  
     ax.scatter(x_data, y_data, color='red')
     
     ax.set_title("Dynamic Scatter Plot")
@@ -23,7 +21,8 @@ def update(frame):
     ax.set_ylim(0, 100)
     ax.grid(True)
 
-# Animate (updates every 1 second)
+
 ani = FuncAnimation(fig, update, interval=1000)
 
 plt.show()
+
